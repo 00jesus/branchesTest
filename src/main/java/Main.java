d0d41feae20f5aa89c461c64333fa964aa9dd603
00jesus <j.acevedo03@ufromail.cl>
2023-04-19 00:55:48 -0400

    Esto va para devJesus

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("clase main");
+        metodoClaseMain();
+    }
+    public static void metodoClaseMain () {
+        System.out.println("metodo 1");
+    }
+}

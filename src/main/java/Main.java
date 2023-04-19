33af77703f9cc7ead89d90456ee9d334e3707c52
00jesus <j.acevedo03@ufromail.cl>
2023-04-19 01:00:02 -0400

    actualizacion del segundo branch, devJesus2 faltantes

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("branch numero 2");
+        metodo2();
+    }
+    public static void metodo2 () {
+        System.out.println("este metodo es de la branch 2");
+    }
+}
